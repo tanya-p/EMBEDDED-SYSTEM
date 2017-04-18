5ca475ba553b750a489e14d2dee0a39f251b6002
tanya-p <56030282@kmitl.ac.th>
2017-04-18 13:32:45 +0700

    Add files via upload

---
+++ Lab1/sos/sos.ino
@@ -0,0 +1,45 @@
+int led = 2;
+void setup() {
+pinMode(2,OUTPUT);
+}
+
+void loop() {
+digitalWrite(led,HIGH);
+delay(1000);
+digitalWrite(led,LOW);
+delay(1000);
+digitalWrite(led,HIGH);
+delay(1000)
+digitalWrite(led,LOW);
+delay(1000)
+digitalWrite(led,HIGH);
+delay(1000)
+digitalWrite(led,LOW);
+delay(1000)
+digitalWrite(led,HIGH);
+delay(3000)
+digitalWrite(led,LOW);
+delay(1000)
+digitalWrite(led,HIGH);
+delay(3000)
+digitalWrite(led,LOW);
+delay(1000)
+digitalWrite(led,HIGH);
+delay(3000)
+digitalWrite(led,LOW);
+delay(1000)
+digitalWrite(led,HIGH);
+delay(1000);
+digitalWrite(led,LOW);
+delay(1000);
+digitalWrite(led,HIGH);
+delay(1000)
+digitalWrite(led,LOW);
+delay(1000)
+digitalWrite(led,HIGH);
+delay(1000)
+digitalWrite(led,LOW);
+delay(1000)
+
+
+}
